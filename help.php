f5ac166ff6a58a12a5dff6ce39861e718b4abaaf
vincent <v1nc3n705@hotmail.com>
2024-03-12 11:17:43 +0100

    improvement

--- help.php
+++ help.php
@@ -47,10 +47,46 @@ require_once 'php_config/link.php';
     </div>
 
     <div class="box">
-        <div class="center">
-            (content)
-        </div>
         (video)
+        <div class="center">
+            <div class="heading">The main page</div>
+            <img src="images/mainpage.png" alt="main page" class="showcase">
+            <p>The starting page with info and announcements. Use the menu to navigate through different pages.</p>
+            <br>
+
+            <div class="heading">Login/Create user</div>
+            <img src="images/login.png" alt="login page" class="showcase">
+            <img src="images/create.png" alt="create page" class="showcase">
+            <p>Where you can create a user and/or login. You only need a username and a password for login, the password will be hashed in the database.</p>
+            <br>
+
+            <div class="heading">Games</div>
+            <img src="images/games.png" alt="games page" class="showcase">
+            <p>Collection of games that are available to play. Click the game you want to play.
+                The instructions for each game is under the game window.
+                The highscore for each game are saved in the database connected to your user.
+                There is a leaderboard at the bottom of each game page related to the game.
+                You can choose to sort/order the leaderboards from what kind of stats are available for each game.
+                This page can only be found and accessed after logging inn.</p>
+            <br>
+
+            <div class="heading">Leaderboards</div>
+            <img src="images/leaderboards.png" alt="leaderboards page" class="showcase">
+            <p>Where you can see leaderboards for every game available here.
+                You can choose to sort/order the leaderboards from what kind of information are available for each game.
+                Your will be highlighted if you are already logged in.</p>
+            <br>
+
+            <div class="heading">FAQ</div>
+            <img src="images/faq.png" alt="faq page" class="showcase">
+            <p>Frequently asked questions about this project/website with answers and additional info.
+                May update with more information and questions in the future.</p>
+            <br>
+
+            <div class="heading">Help Support</div>
+            <p>This is here, where you are now.
+                Explanation and info about the different pages.</p>
+        </div>
     </div>
 
 </div>
